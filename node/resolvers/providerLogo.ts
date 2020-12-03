ee5f2246cd1d8d44d9f8c50cde8e80e630f75cea
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-03 15:45:27 -0400

    Host logo on VTEX infra

--- node/resolvers/providerLogo.ts
+++ node/resolvers/providerLogo.ts
@@ -1,16 +1,30 @@
+/*
+The `src` have two variations, based on weather or not the app is in development (linked).
+-  Development/linked: https://w0geo--checkoutio.myvtex.com/_v/private/assets/v1/linked/vtex.google-geolocation-resolver@0.1.0/public/logo.png
+- Production/released: https://w0geo--checkoutio.myvtex.com/_v/public/assets/v1/published/vtex.google-geolocation-resolver@0.1.0/public/logo.png
+*/
 import { Image } from 'vtex.geolocation-graphql-interface'
-
-// must be the same as assets/logo.png
-const SRC_URL =
-  'https://user-images.githubusercontent.com/26108090/100493516-45802680-3116-11eb-9028-06c336a587a7.png'
 
 const ALT_TEXT = 'Google logo'
 
-const getProviderLogo = async (): Promise<Image> => {
-  return {
-    src: SRC_URL,
-    alt: ALT_TEXT,
-  }
+const getProviderLogo = async (
+  _: unknown,
+  _args: unknown,
+  ctx: Context
+): Promise<Image> => {
+  const {
+    clients: { apps },
+    vtex: { host },
+  } = ctx
+
+  const appId = process.env.VTEX_APP_ID
+  const appManifest = await apps.getApp(appId)
+  const linked = appManifest.version.includes('+')
+  const visibility = linked ? 'private' : 'public'
+  const status = linked ? 'linked' : 'published'
+  const src = `https://${host}/_v/${visibility}/assets/v1/${status}/${appId}/public/logo.png`
+
+  return { src, alt: ALT_TEXT }
 }
 
 export default getProviderLogo
