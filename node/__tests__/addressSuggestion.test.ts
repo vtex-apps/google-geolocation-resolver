2edde007d186b3ab702e04a9e1d5335afdeaf00c
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-24 02:04:30 -0400

    Add and refactor tests

--- node/__tests__/addressSuggestion.test.ts
+++ node/__tests__/addressSuggestion.test.ts
@@ -1,67 +1,24 @@
-import {
-  PlaceAutocompleteRequest,
-  PlaceAutocompleteResponse,
-  Status,
-  PlaceAutocompleteResult,
-} from '@googlemaps/google-maps-services-js'
-import { Apps, IOClients, IOContext } from '@vtex/api'
-import { QueryAddressSuggestionsArgs } from 'vtex.geolocation-graphql-interface'
-
-import { Clients } from '../clients'
-import { Google } from '../clients/Google'
 import getAddressSuggestions from '../resolvers/addressSuggestions'
-
-const mockArgs = (): QueryAddressSuggestionsArgs => ({
-  searchTerm: 'search term',
-  sessionToken: 'session token',
-})
-
-interface MockContextProps {
-  locale: string
-  warn: (warning: unknown) => void
-}
-
-const mockContext = ({ locale, warn }: MockContextProps): Context =>
-  ({
-    clients: {
-      google: {
-        placeAutocomplete: (_request: PlaceAutocompleteRequest) =>
-          Promise.resolve({
-            statusText: Status.OK,
-            data: {
-              predictions: [] as PlaceAutocompleteResult[],
-            },
-          } as PlaceAutocompleteResponse),
-      } as Google,
-      apps: {
-        getAppSettings: (_app: string) =>
-          Promise.resolve({
-            apiKey: 'api key',
-          } as { apiKey: string }),
-      } as Apps,
-    } as IOClients & Clients,
-    vtex: {
-      locale,
-      logger: {
-        warn,
-        error: (_error: unknown) => undefined,
-      },
-    } as IOContext,
-  } as Context)
+// eslint-disable-next-line jest/no-mocks-import
+import {
+  mockContext,
+  defaultWarn,
+  defaultPlaceAutocomplete,
+} from '../__mocks__/context.mock'
 
 describe('addressSuggestion', () => {
   it.each(['pt-BR', 'en'])(
     'shouldn\'t log "%s" as invalid language code',
     async (language: string) => {
-      const mockWarn = jest.fn((_warning: unknown) => undefined)
+      const mockedWarn = jest.fn((_warning: unknown) => undefined)
 
       await getAddressSuggestions(
         {},
-        mockArgs(),
-        mockContext({ locale: language, warn: mockWarn })
+        { searchTerm: '' },
+        mockContext({ locale: language, mockedWarn })
       )
 
-      expect(mockWarn).not.toBeCalledWith(
+      expect(mockedWarn).not.toBeCalledWith(
         `"${language}" is not a valid language. See the list of supported languages on https://developers.google.com/maps/faq#languagesupport`
       )
     }
@@ -69,18 +26,75 @@ describe('addressSuggestion', () => {
 
   it.each(['pt_BR', ''])(
     'should log "%s" as invalid language code',
-    async (language) => {
-      const mockWarn = jest.fn((_warning: unknown) => undefined)
+    async (language: string) => {
+      const mockedWarn = jest.fn((_warning: unknown) => undefined)
 
       await getAddressSuggestions(
         {},
-        mockArgs(),
-        mockContext({ locale: language, warn: mockWarn })
+        { searchTerm: '' },
+        mockContext({ locale: language, mockedWarn })
       )
 
-      expect(mockWarn).toBeCalledWith(
+      expect(mockedWarn).toBeCalledWith(
         `"${language}" is not a valid language. See the list of supported languages on https://developers.google.com/maps/faq#languagesupport`
       )
     }
   )
+
+  it.each(['br', 'bra', 'BR', 'BRA'])(
+    'should restrict address suggestions to Brazil when the "%s" country code is given',
+    async (country: string) => {
+      const mockedWarn = jest.fn(defaultWarn)
+      const mockedPlaceAutocomplete = jest.fn(defaultPlaceAutocomplete)
+
+      await getAddressSuggestions(
+        {},
+        { searchTerm: '', country },
+        mockContext({ mockedPlaceAutocomplete, mockedWarn })
+      )
+
+      expect(mockedPlaceAutocomplete.mock.calls[0][0]).toMatchObject({
+        params: { components: ['country:br'] },
+      })
+      expect(mockedWarn).not.toBeCalledWith(
+        `"${country}" is not a valid country code. Use a two character, case insensitive, ISO 3166-1 Alpha-2 or Alpha-3 compatible country code instead.`
+      )
+    }
+  )
+
+  it('should not restrict address suggestions when no country code is given', async () => {
+    const mockedWarn = jest.fn(defaultWarn)
+    const mockedPlaceAutocomplete = jest.fn(defaultPlaceAutocomplete)
+
+    await getAddressSuggestions(
+      {},
+      { searchTerm: '' },
+      mockContext({ mockedPlaceAutocomplete, mockedWarn })
+    )
+
+    expect(mockedPlaceAutocomplete.mock.calls[0][0]).toMatchObject({
+      params: { components: undefined },
+    })
+    expect(mockedWarn).not.toBeCalledWith(
+      '"" is not a valid country code. Use a two character, case insensitive, ISO 3166-1 Alpha-2 or Alpha-3 compatible country code instead.'
+    )
+  })
+
+  it('should not restrict address suggestions when an invalid country code is given', async () => {
+    const mockedWarn = jest.fn(defaultWarn)
+    const mockedPlaceAutocomplete = jest.fn(defaultPlaceAutocomplete)
+
+    await getAddressSuggestions(
+      {},
+      { searchTerm: '', country: 'Brazil' },
+      mockContext({ mockedPlaceAutocomplete, mockedWarn })
+    )
+
+    expect(mockedPlaceAutocomplete.mock.calls[0][0]).toMatchObject({
+      params: { components: undefined },
+    })
+    expect(mockedWarn).toBeCalledWith(
+      '"Brazil" is not a valid country code. Use a two character, case insensitive, ISO 3166-1 Alpha-2 or Alpha-3 compatible country code instead.'
+    )
+  })
 })
