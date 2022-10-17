d6617893ec2a172c3876ed8fc67175c45c76533d
RobertIviteb <robert@iviteb.com>
2022-10-17 16:53:08 +0300

    Added rules for Romania

--- node/countries/rules.ts
+++ node/countries/rules.ts
@@ -3,12 +3,14 @@ import ARG from './ARG'
 import BOL from './BOL'
 import BRA from './BRA'
 import USA from './USA'
+import ROU from './ROU'
 
 const rules: { [key in ISOAlpha3]?: Rules } = {
   ARG,
   BOL,
   BRA,
   USA,
+  ROU,
 }
 
 export default rules
